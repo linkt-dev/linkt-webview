f299ddb7fabcd9906ac28cff19cf251b89f0b6f6
오현재 <55138387+hangooksaram@users.noreply.github.com>
2024-10-30 19:49:00 +0900

    feat:input-group snippet 추가, search 컴포넌트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,21 @@
 'use client';
 
 import LinkCard from '@/components/ui/link-card';
+import Search from '@/components/ui/search';
 import { mockData } from '@/mockData';
 import { Stack } from '@chakra-ui/react';
 
 export default function App() {
   return (
     <Stack>
-      {mockData.map(({ date, title, description, icon }, index) => {
-        return <LinkCard key={`link-card-${index}`} title={title} date={date} description={description} icon={icon} />;
-      })}
+      <Search />
+      <Stack>
+        {mockData.map(({ date, title, description, icon }, index) => {
+          return (
+            <LinkCard key={`link-card-${index}`} title={title} date={date} description={description} icon={icon} />
+          );
+        })}
+      </Stack>
     </Stack>
   );
 }
---
+++ src/components/ui/search.tsx
@@ -0,0 +1,12 @@
+import { Input } from '@chakra-ui/react';
+import SearchIcon from '@assets/search.svg';
+import { InputGroup } from './input-group';
+import Image from 'next/image';
+
+export default function Search() {
+  return (
+    <InputGroup flex="1" startElement={<Image src={SearchIcon} alt="search-icon" width={19} height={19} />}>
+      <Input placeholder="Search Links ..." bg="primary.main" border="1px solid primary.main" color="white" />
+    </InputGroup>
+  );
+}
