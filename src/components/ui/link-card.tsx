c7604b726f65c4aa63f05001ccaccef2e307cf96
오현재 <55138387+hangooksaram@users.noreply.github.com>
2024-10-30 19:31:19 +0900

    feat:링크 카드 컴포넌트

---
+++ src/components/ui/link-card.tsx
@@ -0,0 +1,43 @@
+import { Box, Card, Center, Text } from '@chakra-ui/react';
+import Image from 'next/image';
+
+export interface ILinkCard {
+  date: string;
+  title: string;
+  description: string;
+  icon: string;
+}
+
+export default function LinkCard(linkCardProps: ILinkCard) {
+  const { date, title, description, icon } = linkCardProps;
+  return (
+    <>
+      <Text color="gray.700" mb="2px">
+        {date}
+      </Text>
+      <Card.Root borderRadius="md" bg="primary.main" color="white" p="8px 8px 8px 4px" flexDir="row" minH="70px">
+        <Center w={{ sm: '80px', base: '80px' }}>
+          <Image width={30} height={30} src={icon} alt="link-type-icon" />
+        </Center>
+
+        <Box w={{ sm: '80%', base: '80%' }}>
+          <Card.Title
+            fontSize="sm"
+            lineHeight="1.2"
+            overflow="hidden"
+            textOverflow="ellipsis"
+            whiteSpace="nowrap"
+            lineClamp={2}
+            mb="4px"
+          >
+            {title}
+          </Card.Title>
+
+          <Card.Description overflow="hidden" textOverflow="ellipsis" whiteSpace="nowrap" color="gray.300">
+            {description}
+          </Card.Description>
+        </Box>
+      </Card.Root>
+    </>
+  );
+}
