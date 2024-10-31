9d16eb38d994ba97da803f3a62332d94a37bcae4
오현재 <55138387+hangooksaram@users.noreply.github.com>
2024-10-31 18:04:58 +0900

    feat:기본 백그라운드, 카드, 텍스트 색상 theme 에 정의

--- src/theme.ts
+++ src/theme.ts
@@ -7,15 +7,61 @@ const config = defineConfig({
         primary: { 
           main : {
             value: "#FFBF1F" , 
-
           },
-          
         },
         white: {
-          value : '#ffffff'
+          value : '#fefefe'
+        },
+        grey : {
+          100 : {
+            value:'#f3f4f6'
+          },
+          200 : {
+            value : '#b0b9c2'
+          },
+          300 : {
+            value : '#3d4450'
+          }
+        },
+        blacked : {
+          100 : {
+            value : '#18171c'
+          },
+          200 : {
+            value : '#101012'
+          }
         }
       },
     },
+    semanticTokens: {
+      colors : {
+        bg : {
+          light : {
+            value : "{colors.grey.100}"
+          },
+          dark : {
+            value : '{colors.blacked.200}'
+          }
+        },
+        card : {
+          light : {
+            value : "{colors.white}"
+          },
+          dark : {
+            value : '{colors.blacked.100}'
+          }
+        },
+        text: {
+          light : {
+            value : "{colors.grey.300}"
+          },
+          dark : {
+            value : "{colors.white}"
+          }
+        }
+      }
+ 
+    }
   },
 })
 
