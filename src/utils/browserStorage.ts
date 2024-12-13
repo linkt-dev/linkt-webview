8fee8aff0bf516b4ab16cefabd30d99d2910d28d
오현재 <55138387+hangooksaram@users.noreply.github.com>
2024-12-13 16:28:58 +0900

    feat:로컬스토리지 데이터 관리하는 메서드들에 typeof window 체크하는 조건 추가

--- src/utils/browserStorage.ts
+++ src/utils/browserStorage.ts
@@ -3,9 +3,14 @@ type LocalStorageDataType<T> = T extends object ? T : string;
 
 export const browserStorage = {
   storeData: (key:string, value: any) => {
-    localStorage.setItem(key, typeof value === 'object' ? JSON.stringify(value) : value);
+    if(typeof window !== undefined){
+      localStorage.setItem(key, typeof value === 'object' ? JSON.stringify(value) : value);
+    }
   },
   getData: <T>(key:string):LocalStorageDataType<T> | null => {
+    if(typeof window === undefined){
+      return null;
+    }
     try {
       const value = localStorage.getItem(key);
       return value === null ? null : JSON.parse(value);
