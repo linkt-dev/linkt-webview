6622c2e04381623c5f62468d0dc0cbdb4696a06f
오현재 <55138387+hangooksaram@users.noreply.github.com>
2024-12-16 14:22:17 +0900

    feat:인증된 요청 보내는 requester 추가

--- src/api/requester.ts
+++ src/api/requester.ts
@@ -1,9 +1,17 @@
-import axios from "axios";
+import { browserStorage } from '@/utils/browserStorage';
+import axios from 'axios';
 
 const API_VERSION = 'v1';
 
 const apiRequester = axios.create({
-    baseURL:`https://api.linkt.one/api/${API_VERSION}`
+  baseURL: `https://api.linkt.one/api/${API_VERSION}`,
+});
+
+export const authorizedApiRequester = axios.create({
+  baseURL: `https://api.linkt.one/api/${API_VERSION}`,
+  headers: {
+    Authorization: `Bearer ${browserStorage.getData('accessToken')}`,
+  },
 });
 
 export default apiRequester;
