70230dc7e33d1186c10742438df151b36cfdf04e
오현재 <55138387+hangooksaram@users.noreply.github.com>
2024-12-16 20:27:46 +0900

    feat:BaseButton borderRadius 변경
    * 25% -> 8px

--- src/components/ui/button/base-button.tsx
+++ src/components/ui/button/base-button.tsx
@@ -8,7 +8,7 @@ export default function BaseButton(props: ButtonProps & RefAttributes<HTMLButton
       bg={{ _light: 'button.light', _dark: 'button.dark' }}
       color={{ _light: 'buttonText.light', _dark: 'buttonText.dark' }}
       _active={{ bg: { _light: 'button.light.active', _dark: 'button.dark.active' } }}
-      borderRadius="25%"
+      borderRadius="8px"
     >
       {props.children}
     </Button>
