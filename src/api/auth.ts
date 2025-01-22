1e84453b0e033075d828a3a82235826d546ff5d2
오현재 <55138387+hangooksaram@users.noreply.github.com>
2025-01-22 15:32:37 +0900

    fix:로그인 요청 시, withCredenitals:true 속성 추가

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,11 +1,13 @@
-import apiRequester from './requester';
+import { authorizedApiRequester } from './requester';
 
 interface AuthResponse {
   accessToken: string;
 }
 
-export const checkAuth = async (): Promise<AuthResponse> => {
-  const { data } = await apiRequester.post<AuthResponse>('/auth');
+export const checkAuth = async (userId: string): Promise<AuthResponse> => {
+  const { data } = await authorizedApiRequester.post<AuthResponse>('/auth', {
+    userId: userId,
+  });
 
   return data;
 };
