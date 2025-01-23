ab032e32ad31bde059f5fbf61668910ab3245ae0
오현재 <55138387+hangooksaram@users.noreply.github.com>
2025-01-23 17:23:14 +0900

    feat:baseApiUrl 개발환경에따라 환경변수 참조하도록

--- src/api/requester.ts
+++ src/api/requester.ts
@@ -1,13 +1,17 @@
 import axios from 'axios';
 
 const API_VERSION = 'v1';
+const baseApiUrl =
+  process.env.NODE_ENV === 'development'
+    ? process.env.NEXT_PUBLIC_DEV_API_URL
+    : process.env.NEXT_PUBLIC_PRODUCTION_API_URL;
 
 const apiRequester = axios.create({
-  baseURL: `https://dev-api.linkt.one/api/${API_VERSION}`,
+  baseURL: `${baseApiUrl}/${API_VERSION}`,
 });
 
 export const authorizedApiRequester = axios.create({
-  baseURL: `https://dev-api.linkt.one/api/${API_VERSION}`,
+  baseURL: `${baseApiUrl}/${API_VERSION}`,
   withCredentials: true,
 });
 
